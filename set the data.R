860670d739f8c6fac02cd70c59bc24813fff8444
Jason Oke <75785520+jokeyjo@users.noreply.github.com>
2021-11-19 14:40:40 +0000

    File with basic data management

---
+++ set the data.R
@@ -0,0 +1,33 @@
+# Set the data 
+library(MRMCaov)
+library(data.table)
+library(ggplot2)
+library(pROC)
+library(flextable)
+library(magrittr)
+library(boot)
+library(officer)
+
+## Read in the Optellum data 
+source("src/functions.R")
+
+rr = fread("data/reader_responses_updated.csv")
+pt = fread("data/patient_info_updated(4-23-21).csv")
+
+# This bit recodes the scores so that they can "stacked"
+s1 = names(rr)[-6]
+s2 = names(rr)[-3]
+rrlm1 = rr[,..s1]
+setnames(rrlm1,old = "LOM1",new = "rating")
+rrlm1[,test:="LOM1"]
+
+rrlm2 = rr[,..s2]
+setnames(rrlm2,old = "LOM2",new = "rating")
+rrlm2[,test:="LOM2"]
+
+rrb = rbind(rrlm1,rrlm2)
+setkey(rrb,reader)
+
+## Now merge with the patient file - merging on index. 
+DT = merge(rrb,pt,by = "index")
+
