fffe93bd099fa27d4849d6a95849d478e5f8d8fc
Jason Oke <75785520+jokeyjo@users.noreply.github.com>
2021-12-08 11:50:46 +0000

    Part solid Change to the classification

--- set the data.R
+++ set the data.R
@@ -11,8 +11,9 @@ library(officer)
 ## Read in the Optellum data 
 source("src/functions.R")
 
-rr = fread("data/reader_responses_updated.csv")
-pt = fread("data/patient_info_updated(4-23-21).csv")
+rr <- fread("data/reader_responses_updated.csv")
+ps <- fread("data/partsolid_updated_dataset_jason(11-23-21).csv")
+pt <- fread("data/patient_info_updated(4-23-21).csv")
 
 # This bit recodes the scores so that they can "stacked"
 s1 = names(rr)[-6]
@@ -30,4 +31,13 @@ setkey(rrb,reader)
 
 ## Now merge with the patient file - merging on index. 
 DT = merge(rrb,pt,by = "index")
+
+# 55 cases in previous analysis 
+DT[,isMixed[1], by = index][,table(V1)] 
+
+# as per email from Roger Kim Tue 23/11/2021
+DT[,partSolid := ps$partsolid[match(DT$index,ps$index)]]
+
+# 31 moved from solid to part-solid
+DT[,.(isMixed[1],partSolid[1]), by = index][,table(V1,V2)] 
 
